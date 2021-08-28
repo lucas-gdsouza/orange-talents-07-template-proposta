273fd9448902cbd921896b1abc67752d08943155
Lucas Souza <lucas.souza@zup.com.br>
2021-08-28 14:14:32 -0300

    Login via senha do cartão

---
+++ propostas/src/main/java/br/com/zupacademy/propostas/customizations/security/ResourceServerConfig.java
@@ -0,0 +1,25 @@
+package br.com.zupacademy.propostas.customizations.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_escopo-proposta")
+                        .antMatchers(HttpMethod.GET, "/api/v1/propostas/**").hasAuthority("SCOPE_escopo-proposta")
+                        .antMatchers(HttpMethod.POST, "/api/v1/biometrias/**").hasAuthority("SCOPE_escopo-proposta")
+                        .antMatchers(HttpMethod.POST, "/api/v1/propostas/**").hasAuthority("SCOPE_escopo-proposta")
+                        .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
