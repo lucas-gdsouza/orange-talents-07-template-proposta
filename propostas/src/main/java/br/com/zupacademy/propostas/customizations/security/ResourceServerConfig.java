d570cae0934be26fed71a47bd35c002c6c810034
Lucas Souza <lucas.souza@zup.com.br>
2021-08-30 16:54:51 -0300

    Habilitando Métricas - 4.11.15.1

--- propostas/src/main/java/br/com/zupacademy/propostas/customizations/security/ResourceServerConfig.java
+++ propostas/src/main/java/br/com/zupacademy/propostas/customizations/security/ResourceServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -22,5 +23,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                         .antMatchers(HttpMethod.POST, "/api/v1/propostas/**").hasAuthority("SCOPE_escopo-proposta")
                         .anyRequest().authenticated()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
